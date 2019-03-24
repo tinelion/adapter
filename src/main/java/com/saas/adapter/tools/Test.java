a16ec033d008101f00b8a8f4fc8163a4e54746ed
OldCity <1161260131@qq.com>
2019-03-24 19:36:43 +0800

    测试分支

--- src/main/java/com/saas/adapter/tools/Test.java
+++ src/main/java/com/saas/adapter/tools/Test.java
@@ -7,6 +7,6 @@ package com.saas.adapter.tools;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("测试分支");
+        System.out.println("再次测试");
     }
 }
